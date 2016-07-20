38ff3b82070823d414ba67976923e734fcdd3fbf
sunke <sunkejl@live.com>
2016-07-20 16:01:05 +0800

    refs command

---
+++ index1.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: sunke
+     * Date: 2016/7/20
+     * Time: 15:37
+     */
+    set_time_limit(0);
+    for ($i = 0 ; $i < 10 ; $i++) {
+        echo $i;
+        sleep(2);
+    }
